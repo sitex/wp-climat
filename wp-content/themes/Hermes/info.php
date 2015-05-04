4296d32d15105e20b490818002da5b80febb9edb
Ivan Samorodov <gsitex@gmail.com>
2015-05-05 02:48:06 +0800

    gree info

--- wp-content/themes/Hermes/info.php
+++ wp-content/themes/Hermes/info.php
@@ -6,6 +6,9 @@ if (in_array($_SERVER["REQUEST_URI"], ['', '/'])) {
 if (strpos($_SERVER["REQUEST_URI"], 'uslugi') !== false) {
 	$show_info = TEMPLATEPATH . '/info_uslugi.php';
 }
+if (strpos($_SERVER["REQUEST_URI"], 'portfoliosets/gree') !== false) {
+	$show_info = TEMPLATEPATH . '/info_gree.php';
+}
 // show
 if ($show_info) {
 	include $show_info;
