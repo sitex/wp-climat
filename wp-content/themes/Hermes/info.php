ab9788db2eaa3275c97983082e1c75c4b104c1e5
Ivan Samorodov <gsitex@gmail.com>
2015-05-07 19:52:44 +0800

    hide GREE extra info

--- wp-content/themes/Hermes/info.php
+++ wp-content/themes/Hermes/info.php
@@ -6,9 +6,9 @@ if (in_array($_SERVER["REQUEST_URI"], array('', '/'))) {
 if (strpos($_SERVER["REQUEST_URI"], 'uslugi') !== false) {
 	$show_info = TEMPLATEPATH . '/info_uslugi.php';
 }
-if (strpos($_SERVER["REQUEST_URI"], 'portfoliosets/gree') !== false) {
-	$show_info = TEMPLATEPATH . '/info_gree.php';
-}
+//if (strpos($_SERVER["REQUEST_URI"], 'portfoliosets/gree') !== false) {
+//	$show_info = TEMPLATEPATH . '/info_gree.php';
+//}
 // show
 if ($show_info) {
 	include $show_info;
