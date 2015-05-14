607ca8ea301b8b3cc8b07a97bc21a393e8ae9407
Ivan Samorodov <gsitex@gmail.com>
2015-05-14 16:33:19 +0800

    remove uslugi info

--- wp-content/themes/Hermes/info.php
+++ wp-content/themes/Hermes/info.php
@@ -3,9 +3,9 @@ $show_info = false;
 //if (in_array($_SERVER["REQUEST_URI"], array('', '/'))) {
 //	$show_info = TEMPLATEPATH . '/info_main.php';
 //};
-if (strpos($_SERVER["REQUEST_URI"], 'uslugi') !== false) {
-	$show_info = TEMPLATEPATH . '/info_uslugi.php';
-}
+//if (strpos($_SERVER["REQUEST_URI"], 'uslugi') !== false) {
+//	$show_info = TEMPLATEPATH . '/info_uslugi.php';
+//}
 //if (strpos($_SERVER["REQUEST_URI"], 'portfoliosets/gree') !== false) {
 //	$show_info = TEMPLATEPATH . '/info_gree.php';
 //}
